befd2a365f4d8b2bd8de6ed849909f9699ba0348
ThathamsettySirisha <122029709+ThathamsettySirisha@users.noreply.github.com>
2023-09-18 17:09:00 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivitiesRunnerForactivity4.java
@@ -0,0 +1,15 @@
+package Activities;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Features",
+        glue = {"stepDefinitions"},
+        tags = "@activity4"
+)
+
+public class ActivitiesRunnerForactivity4 {
+}
